db093133298fe0dedf0dd82156439deb9fc0c959
Miguel Carrasco <mlacarrasco@gmail.com>
2025-01-24 21:02:34 -0300

    updates

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,4 @@
-# wsgi.py
-import sys
-
-# add your project directory to the sys.path
-project_home = '/app'
-if project_home not in sys.path:
-    sys.path = [project_home] + sys.path
-
-# Import flask app and create 'app' variable for Render
-from app.flask_app import app as application
-app = application  # Add this line to make it work with Render
+from app import app, socketio
 
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app)
